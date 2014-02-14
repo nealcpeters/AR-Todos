a01f02ec332a3d5d467c1403e37d38ade3176b1b
BATMAN <apprentice@dbc-chicago-34.local>
2014-02-14 10:06:46 -0600

    Modified todo.rb and todo_list.rb controller file

---
+++ app/controllers/todo_list.rb
@@ -0,0 +1,9 @@
+require_relative '../models/task.rb'
+require_relative '../../config/application.rb'
+
+
+class ToDoList
+
+
+
+end
--- todo.rb
+++ todo.rb
@@ -1,3 +1,6 @@
-require_relative 'config/application'
+sublrequire_relative 'config/application'
+require_relative 'app/controllers/todo_list'
+
 
 puts "Put your application code in #{File.expand_path(__FILE__)}"
+
