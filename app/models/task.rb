f7e9f94bd9802dad074bd3722b9676acedd4c7ac
Apprentice <apprentice@dbc-chicago-026.local>
2014-02-14 10:58:39 -0600

    added some methods to Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,38 @@
 class Task < ActiveRecord::Base
 
+  def self.grab_task(id)
+    self.find(id)
+  end
 
+  def self.show_completed
+    self.where(complete: 1).each_with_index { |task| puts }
+  end
 
+  def self.show_uncompleted
+    self.where(complete: 0).each { |task| puts }
+  end
+
+  def complete_task
+    self.complete = 1
+    save
+  end
+
+  def uncomplete_task
+    self.complete = 0
+    save
+  end
+
+  def delete_task(id)
+    self.find(id).destroy
+  end
+
+  def update_content(text)
+    self.contents = text
+    save
+  end
+
+  def to_s
+    compl = complete == 0 ?  " " : "X"
+    "[#{compl}] #{contents}"
+  end
 end
